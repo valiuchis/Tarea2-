f07b265c226273eb2112c490b534df8462fad710
MatiasMendozaa <mmendoza2019@udec.cl>
2022-10-28 14:20:55 -0300

    Actualizacion de la clase Bebida

--- TareaProg2/src/Bebida.java
+++ TareaProg2/src/Bebida.java
@@ -1,19 +1,51 @@
 
-abstract class Bebida {
-    
-    private int serie;
-    
-    public Bebida (int serie) {
-        this.serie = serie;
+abstract class Bebida{
+
+    private int Serie;
+
+    public Bebida(int s){
+        Serie = s;
     }
-    
-    public int getSerie () {
-        return serie;
+
+    public int getSerie() {
+        return Serie;
     }
-    
-    public String Beber () {
-        String s = new String ("Bebida: ");
-        return s;
+
+    public String beber(){
+        return "sabor: ";
     }
-    
+
+}
+
+class Sprite extends Bebida {
+
+    public Sprite(int Serie) {
+        super(Serie);
+    }
+    public String beber(){
+        return super.beber() + "sprite";
+
+    }
+}
+
+class CocaCola extends Bebida {
+
+    public CocaCola(int Serie) {
+        super(Serie);
+    }
+
+    public String beber(){
+        return super.beber() + "cocacola";
+    }
+}
+
+class Fanta extends Bebida {
+
+    public Fanta(int Serie) {
+        super(Serie);
+    }
+
+    public String beber(){
+        return super.beber() + "fanta";
+    }
 }
