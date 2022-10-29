9fb8d228b4221cb77a0a05488016b3bc7c49a105
valiuchis <valeriacrc1301@outlook.com>
2022-10-29 14:47:54 -0300

    Clase Cliente (Modificada)

--- TareaProg2/src/Cliente.java
+++ TareaProg2/src/Cliente.java
@@ -1,12 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author valeriacarolinaramos
- */
 public class Cliente {
+    
+    private Moneda pago;
+    private int id;
+    private Expendedora vendingMachine;
+    private Bebida bebida;
+    
+    public Cliente(Moneda moneda, int codigoID, Expendedora exp) {
+        
+        pago = moneda;
+        id = codigoID;
+        vendingMachine = exp;
+    }
+    
+    public Bebida compraBebida () {
+        
+        try { 
+            bebida = vendingMachine.ventaBebida(pago, id);
+        } catch (NoHayBebidaException exception) {
+            System.out.println(exception.getMessage());
+            
+        } catch (PagoIncorrectoException exception) {
+            System.out.println(exception.getMessage());
+            
+        } catch (PagoInsuficienteException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return bebida;    
+    }
+    
+    public String saborBebida () {
+        return bebida.beber();
+    }
     
 }
