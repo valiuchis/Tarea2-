afc39a1d17d637ec0ef26ddd1f096b164b6d38e8
valiuchis <valeriacrc1301@outlook.com>
2022-10-30 22:37:42 -0300

    Ejemplos en Main

--- TareaProg2/src/tareaprog2/TareaProg2.java
+++ TareaProg2/src/tareaprog2/TareaProg2.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package tareaprog2;
 
-/**
- *
- * @author valeriacarolinaramos
- */
 public class TareaProg2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Moneda cien = new MonedaCien();
+        Expendedora maquinaExpendedora = new Expendedora(6,1000);
+        Cliente valeria = new Cliente(cien,2,maquinaExpendedora);
+        valeria.compraBebida();
+        System.out.println(valeria.saborBebida());
+        System.out.println(maquinaExpendedora.calcularVuelto());
+   
     }
     
 }
