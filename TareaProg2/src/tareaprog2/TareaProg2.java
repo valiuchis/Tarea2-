5f2a2c03d0e167606b3b262985bbfa4b922631c7
valiuchis <valeriacrc1301@outlook.com>
2022-10-30 22:44:38 -0300

    Segundo Ejemplo en Main

--- TareaProg2/src/tareaprog2/TareaProg2.java
+++ TareaProg2/src/tareaprog2/TareaProg2.java
@@ -4,12 +4,23 @@ package tareaprog2;
 public class TareaProg2 {
 
     public static void main(String[] args) {
+        
+        System.out.println("Caso 1:");
+        Moneda mil = new MonedaMil();
+        Expendedora maquinaExpendedora2 = new Expendedora(3,500);
+        Cliente Matias = new Cliente(mil,1,maquinaExpendedora2);
+        Matias.compraBebida();
+        System.out.println(Matias.saborBebida());
+        System.out.println("El vuelto es: " + maquinaExpendedora2.calcularVuelto() + "$");
+        
+        System.out.println("Caso 2:");
         Moneda cien = new MonedaCien();
         Expendedora maquinaExpendedora = new Expendedora(6,1000);
-        Cliente valeria = new Cliente(cien,2,maquinaExpendedora);
-        valeria.compraBebida();
-        System.out.println(valeria.saborBebida());
+        Cliente Valeria = new Cliente(cien,2,maquinaExpendedora);
+        Valeria.compraBebida();
+        System.out.println(Valeria.saborBebida());
         System.out.println(maquinaExpendedora.calcularVuelto());
+        
    
     }
     
